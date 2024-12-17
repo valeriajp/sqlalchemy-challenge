f1271876071a514c50a8660177cb151a27a5367d
valeriajp <vjimpaz@gmail.com>
2024-12-17 17:17:12 -0500

    Update app.py

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -13,8 +13,6 @@ from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 from flask import Flask, jsonify
 
-from flask import Flask, jsonify
-
 #################################################
 # Database Setup
 #################################################
